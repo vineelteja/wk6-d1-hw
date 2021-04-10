f04964bac16bd71ebb8d8d8862a8cef6aa768e91
vineel <email.com>
2021-04-10 02:18:49 -0700

    first

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname));
+const bodyParser = require("body-parser");
+const expressSession = require("express-session")({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("App listening on port: ", port));
+
+const passport = require("passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+mongoose.connect(`mongodb://mongo_camp/MyDatabase`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+  username: String,
+  password: String,
+});
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model("userInfo", UserDetail, "userInfo");
+
+passport.use(UserDetails.createStrategy());
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+//  ROUTES
+
+const connectEnsureLogin = require("connect-ensure-login");
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect("/login?info=" + info);
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/");
+    });
+  })(req, res, next);
+});
+
+app.get("/login", (req, res) =>
+  res.sendFile("html/login.html", { root: __dirname })
+);
+app.get("/", connectEnsureLogin.ensureLoggedIn(), (req, res) =>
+  res.sendFile("html/index.html", { root: __dirname })
+);
+app.get("/private", connectEnsureLogin.ensureLoggedIn(), (req, res) =>
+  res.sendFile("html/private.html", { root: __dirname })
+);
+app.get("/user", connectEnsureLogin.ensureLoggedIn(), (req, res) =>
+  res.send({ user: req.user })
+);
+app.get("/logout", (req, res) => {
+  req.logout(), res.sendFile("html/logout.html", { root: __dirname });
+});
+
+// UserDetails.register({ username: "paul", active: false }, "paul");
+// UserDetails.register({ username: "joy", active: false }, "Joy");
+// UserDetails.register({ username: "ray", active: false }, "ray");
